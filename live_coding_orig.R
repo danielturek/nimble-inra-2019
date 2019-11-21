43f5bf1bf0817da28b5334cc11b2cb13c0f2cde5
danielturek <danielturek@gmail.com>
2019-11-21 14:06:15 +0100

    updates

--- live_coding_orig.R
+++ live_coding_orig.R
@@ -76,7 +76,7 @@ Rmodel$getLogProb('p')  # now we're good
 
 
 ##
-## Part 2: Improving MCMC Performance
+## Part 2: MCMC Performance
 ##
 
 ## slice sampling for litters model:
@@ -86,138 +86,6 @@ Rmodel$getLogProb('p')  # now we're good
 ## block sampling with initial covariance matrices:
 
 
-
-
-
-
-
-
-
-
-## Dipper CJS model:
-
-library(nimble)
-load('~/Downloads/dipper_data.Rdata')
-
-
-
-
-
-
-
-
-
-
-######################################
-######################################
-######################################
-######        Day 2     ##############
-######################################
-######################################
-######################################
-
-
-
-
-##
-## Session 3: nimbleEcology R Package
-##
-
-## (first) original dipper model:
-
-library(nimble)
-load('~/Downloads/dipper_data.Rdata')
-
-
-code <- nimbleCode({
-    ## priors
-    phi ~ dunif(0, 1)
-    p ~ dunif(0, 1)
-    for(i in 1:N) {
-        ## condition on first observations
-        x[i, first[i]] <- 1
-        y[i, first[i]] <- 1
-        for(j in (first[i]+1):T) {
-            x[i, j] ~ dbern(phi * x[i, j-1])
-            y[i, j] ~ dbern(  p * x[i, j])
-        }
-    }
-})
-
-N <- dim(sightings)[1]
-T <- dim(sightings)[2]
-
-constants <- list(N = N, T = T, first = first)
-
-data <- list(y = sightings)
-
-xInit <- sightings
-xInit[xInit == 0] <- 1
-
-inits <- list(phi = 0.5,
-              p = 0.5,
-              x = xInit)
-
-Rmodel <- nimbleModel(code, constants, data, inits)
-Rmodel$calculate()   ## -1111.808
-
-conf <- configureMCMC(Rmodel)
-
-Rmcmc <- buildMCMC(conf)
-
-Cmodel <- compileNimble(Rmodel)
-Cmcmc <- compileNimble(Rmcmc, project = Rmodel)
-
-samples <- runMCMC(Cmcmc, 20000, nburnin = 10000)
-
-
-## now: modify dipper model to use dCJS_ss:
-
-
-
-## finally: compare latent and dCJS models
-## using compareMCMCs()
-## renameMCMC(comp, newName, oldName)
-## make_MCMC_comparison_pages(comp, dir, modelName)
-
-## argument: pageComponents
-## list(timing = FALSE, efficiencySummary = FALSE, 
-##             efficiencySummaryAllParams = TRUE, paceSummaryAllParams = TRUE, 
-##             efficiencyDetails = TRUE, posteriorSummary = TRUE)
-
-
-
-
-## Occupancy model:
-
-library(nimble)
-
-load('~/Downloads/occupancy_data.Rdata')
-
-
-
-## using dOcc_v(probOcc, probDetect, len) distribution
-
-
-
-
-
-
-##
-## Session 4: Programming and Advanced Uses of NIMBLE
-##
-
-
-## ML estimations for the
-## marginalized occupancy model
-
-
-load('~/Downloads/occupancy_data.Rdata')
-
-
-
-
-## optim(par, function, control = list(fnscale = -1))
 
 
 
